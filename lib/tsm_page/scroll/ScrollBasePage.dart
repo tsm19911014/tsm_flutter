47695fd8f7a471927392d918aa554aa5204db9d9
tsm <tian.shm@neusoft.com>
2020-08-20 16:19:56 +0800

    ListView  学习

--- lib/tsm_page/scroll/ScrollBasePage.dart
+++ lib/tsm_page/scroll/ScrollBasePage.dart
@@ -6,25 +6,28 @@ import '../../tsm_page_routes.dart';
 class ScrollBasePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    List<String> list = ["SingleChildScrollView", 'GridView'];
+    List<String> list = ["SingleChildScrollView", 'GridView',"ListView"];
     return Scaffold(
       appBar: AppBar(
         title: Text('ScrollView 集合'),
         centerTitle: true,
       ),
-      body: ListView.builder(
-        physics: BouncingScrollPhysics(),
-        itemCount: list.length,
-        itemBuilder: (context, index) => LayoutBuilder(
-          builder: (context, cons) => SizedBox(
-            width: cons.maxWidth,
-            child: RaisedButton(
-              child: Text(list[index]),
-              onPressed: () => clickItem(context, index),
+      body: Scrollbar(
+        child: ListView.separated(
+          physics: BouncingScrollPhysics(),
+          itemCount: list.length,
+          separatorBuilder: (context,index)=>Divider(),
+          itemBuilder: (context, index) => LayoutBuilder(
+            builder: (context, cons) => SizedBox(
+              width: cons.maxWidth,
+              child: FlatButton(
+                child: Text(list[index]),
+                onPressed: () => clickItem(context, index),
+              ),
             ),
           ),
         ),
-      ),
+      )
     );
   }
 
@@ -35,6 +38,9 @@ class ScrollBasePage extends StatelessWidget {
         break;
       case 1:
         Navigator.of(context).pushNamed(page_routes_grid_view);
+        break;
+      case 2:
+        Navigator.of(context).pushNamed(page_routes_list_view);
         break;
     }
   }
--- lib/tsm_page/scroll/TsmListViewPage.dart
+++ lib/tsm_page/scroll/TsmListViewPage.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_app1/utils.dart';
+import 'package:flutter_app1/widget/ViewInflate.dart';
 
 class TsmListViewPage extends StatefulWidget {
   @override
@@ -7,12 +9,110 @@ class TsmListViewPage extends StatefulWidget {
 }
 
 class _TsmListViewPage extends State<TsmListViewPage> {
+  List<String> list_str;
+  String endTag;
+
+  List<String> page = [
+    '条目1',
+    '条目2',
+    '条目3',
+    '条目4',
+    '条目5',
+    '条目6',
+    '条目7',
+    '条目8',
+    '条目9',
+    '条目10'
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('ListView 学习'),
         centerTitle: true,
+      ),
+      body: getBody(),
+    );
+  }
+
+  getBody() {
+    if ((list_str ??= []).length == 0) {
+      return getEmptyView();
+    }
+    return getListBody();
+  }
+
+  getListBody() {
+    if (list_str.length < 100) {
+      loadDate();
+    }
+    return ListView.separated(
+        itemCount: list_str.length,
+        separatorBuilder: (context, index) => Divider(),
+        itemBuilder: (context, index) {
+          if (list_str[index] == endTag) {
+            return Container(
+              padding: const EdgeInsets.all(16.0),
+              alignment: Alignment.center,
+              child: SizedBox(
+                  width: 24.0,
+                  height: 24.0,
+                  child: CircularProgressIndicator(strokeWidth: 2.0)),
+            );
+          } else {
+            return Container(
+              width: double.infinity,
+              alignment: Alignment.center,
+              padding: const EdgeInsets.all(10),
+              child:inflateText(list_str[index], Colors.blueAccent, 16),
+            );
+          }
+        });
+  }
+
+  loadDate() {
+    Future.delayed(Duration(seconds: 2)).then((e) {
+      setState(() {
+        list_str ??= [];
+        if(list_str.length==0){
+          list_str.add(endTag);
+        }
+        list_str.insertAll(list_str.length-1, page);
+
+        if(list_str.length>100){
+          list_str.removeAt(list_str.length-1);
+          list_str.add("没有更多条目了");
+        }
+      });
+    });
+  }
+
+  getEmptyView() {
+    return Container(
+      color: Colors.white,
+      child: Center(
+        child: GestureDetector(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Icon(Icons.adjust),
+              SizedBox(
+                height: 10,
+              ),
+              inflateText('点击刷新', Colors.redAccent, 14),
+            ],
+          ),
+          onTap: () {
+            loadDate();
+            printString("开始刷新");
+          },
+        ),
       ),
     );
   }
