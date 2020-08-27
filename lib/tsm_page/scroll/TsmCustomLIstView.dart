d9632faca83a7f7bebf33bb26027414ceb06dc55
tsm <tian.shm@neusoft.com>
2020-08-27 16:26:33 +0800

    下拉刷新与CustomScrollView  学习

--- lib/tsm_page/scroll/TsmCustomLIstView.dart
+++ lib/tsm_page/scroll/TsmCustomLIstView.dart
@@ -18,6 +18,42 @@ class _TsmCustomScrollViewState extends State<TsmCustomScrollViewPage> {
 
         ///越界回弹效果
         slivers: [
+          CupertinoSliverRefreshControl(
+            refreshIndicatorExtent: 60,
+
+            /// 刷新过程中悬浮高度
+            refreshTriggerPullDistance: 100,
+
+            ///触发刷新的距离
+            /// 自定义布局
+            builder: (context, buildRefreshindictor, pulledExtent,
+                refreshTriggerPullDistance, refreshIndicatorExtent) {
+              printString(
+                  'pulledExtent : ${pulledExtent}   ,refreshTriggerPullDistance  : ${refreshTriggerPullDistance}    refreshIndicatorExtent:${refreshIndicatorExtent}');
+              return Container(
+                color: Colors.redAccent,
+                height: 150,
+                alignment: Alignment.center,
+                child: AnimatedOpacity(
+                    duration: Duration(milliseconds: 300),
+                    //opacity: top == 80.0 ? 1.0 : 0.0,
+                    opacity: 1.0,
+                    child: Text(
+                      RefreshIndicatorMode.done == buildRefreshindictor
+                          ? '已拉动:${pulledExtent.round()}  松开刷新'
+                          : '已拉动:${pulledExtent.round()}  下拉刷新',
+                      style: TextStyle(fontSize: 12.0),
+                    )),
+              );
+            },
+
+            ///触发刷新回调
+            onRefresh: () async {
+              await Future.delayed(Duration(seconds: 3));
+              printString('CupertinoSliverRefreshControl   onRefresh');
+            },
+          ),
+
           SliverAppBar(
             centerTitle: true,
             titleSpacing: 0,
@@ -25,67 +61,57 @@ class _TsmCustomScrollViewState extends State<TsmCustomScrollViewPage> {
               'CustomScrollView',
               style: TextStyle(color: Colors.black87),
             ),
-            snap: false,
 
             /// 停止拖动是否 继续响应拖动事件
-            floating: true,
+            snap: false,
 
-            ///floating(浮动) 控制展开的，
+            /// 下滑过程中是否优先展开头部折叠部分 ,
+            floating: false,
+
+            ///pinned(固定的)  控制隐藏和展示的   false 就是可以随列表滑出屏幕外  true 不可以
             pinned: true,
-            onStretchTrigger: () {
-              printString('onStretchTrigger');
-              return;
-            },
-
-            ///pinned(固定的)  控制隐藏和展示的   false 就是可以滑出屏幕外  true 不可以
-            flexibleSpace: LayoutBuilder(builder: (context,cons){
-
-              return FlexibleSpaceBar(
-//              collapseMode: CollapseMode.pin,
-                  background: Container(
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                          image: AssetImage('images/hor.jpg'), fit: BoxFit.fill),
-                    ),
-                    child: Column(
-                      children: [
-                        SizedBox(
-                          height: 70,
-                        ),
-                        Text(
-                          '这个是头部',
-                          style: TextStyle(color: Colors.white),
-                        ),
-                        Text(
-                          '这个是头部',
-                          style: TextStyle(color: Colors.white),
-                        ),
-                        Text(
-                          '这个是头部',
-                          style: TextStyle(color: Colors.white),
-                        ),
-                        Text(
-                          '这个是头部',
-                          style: TextStyle(color: Colors.white),
-                        ),
-                        Text(
-                          '这个是头部',
-                          style: TextStyle(color: Colors.white),
-                        ),
-                      ],
-                    ),
-                  ));
-            },),
             backgroundColor: Colors.blueAccent,
-            expandedHeight: 200,
+            expandedHeight: 200,///展开后的高度
+            flexibleSpace: LayoutBuilder(
+              builder: (context, cons) {
+                return FlexibleSpaceBar(
+//              collapseMode: CollapseMode.pin,
+                    background: Container(
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage('images/hor.jpg'), fit: BoxFit.fill),
+                  ),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.max,
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Text(
+                        '这个是头部',
+                        style: TextStyle(color: Colors.pink),
+                      ),
+                      Text(
+                        '这个是头部',
+                        style: TextStyle(color: Colors.pink),
+                      ),
+                      Text(
+                        '这个是头部',
+                        style: TextStyle(color: Colors.pink),
+                      ),
+                      Text(
+                        '这个是头部',
+                        style: TextStyle(color: Colors.pink),
+                      ),
+                      Text(
+                        '这个是头部',
+                        style: TextStyle(color: Colors.pink),
+                      ),
+                    ],
+                  ),
+                ));
+              },
+            ),
           ),
-          CupertinoSliverRefreshControl(
-            refreshIndicatorExtent: 100,
-            refreshTriggerPullDistance: 140,
-            onRefresh: () async {
-              printString('CupertinoSliverRefreshControl   onRefresh');
-            },
-          ),
+//
           SliverPersistentHeader(
             floating: true,
             pinned: false,
