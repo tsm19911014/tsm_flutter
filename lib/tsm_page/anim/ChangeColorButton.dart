6c6313c95c4aaafcf96d8923d1b5e657a1195722
tsm <tian.shm@neusoft.com>
2020-09-09 11:18:29 +0800

    动画Animation  学习

--- lib/tsm_page/anim/ChangeColorButton.dart
+++ lib/tsm_page/anim/ChangeColorButton.dart
@@ -3,7 +3,6 @@
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_app1/utils.dart';
 
 class ChangeColorButton extends StatefulWidget{
 
--- lib/tsm_page/anim/TsmStaggerAnimationPage.dart
+++ lib/tsm_page/anim/TsmStaggerAnimationPage.dart
@@ -1,5 +1,133 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_app1/tsm_page/anim/TsmColorAnimatedDecorated.dart';
+
+class AnimatedWidgetsTest extends StatefulWidget {
+@override
+_AnimatedWidgetsTestState createState() => _AnimatedWidgetsTestState();
+}
+
+class _AnimatedWidgetsTestState extends State<AnimatedWidgetsTest> {
+  double _padding = 10;
+  var _align = Alignment.topRight;
+  double _height = 100;
+  double _left = 0;
+  Color _color = Colors.red;
+  TextStyle _style = TextStyle(color: Colors.black);
+  Color _decorationColor = Colors.blue;
+
+  @override
+  Widget build(BuildContext context) {
+    var duration = Duration(seconds: 5);
+    return SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          RaisedButton(
+            onPressed: () {
+              setState(() {
+                _padding = 20;
+              });
+            },
+            child: AnimatedPadding(
+              duration: duration,
+              padding: EdgeInsets.all(_padding),
+              child: Text("AnimatedPadding"),
+            ),
+          ),
+          SizedBox(
+            height: 50,
+            child: Stack(
+              children: <Widget>[
+                AnimatedPositioned(
+                  duration: duration,
+                  left: _left,
+                  child: RaisedButton(
+                    onPressed: () {
+                      setState(() {
+                        _left = 100;
+                      });
+                    },
+                    child: Text("AnimatedPositioned"),
+                  ),
+                )
+              ],
+            ),
+          ),
+          Container(
+            height: 100,
+            color: Colors.grey,
+            child: AnimatedAlign(
+              duration: duration,
+              alignment: _align,
+              child: RaisedButton(
+                onPressed: () {
+                  setState(() {
+                    _align = Alignment.center;
+                  });
+                },
+                child: Text("AnimatedAlign"),
+              ),
+            ),
+          ),
+          AnimatedContainer(
+            duration: duration,
+            height: _height,
+            color: _color,
+            child: FlatButton(
+              onPressed: () {
+                setState(() {
+                  _height = 150;
+                  _color = Colors.blue;
+                });
+              },
+              child: Text(
+                "AnimatedContainer",
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ),
+          AnimatedDefaultTextStyle(
+            child: GestureDetector(
+              child: Text("hello world"),
+              onTap: () {
+                setState(() {
+                  _style = TextStyle(
+                    color: Colors.blue,
+                    decorationStyle: TextDecorationStyle.solid,
+                    decorationColor: Colors.blue,
+                  );
+                });
+              },
+            ),
+            style: _style,
+            duration: duration,
+          ),
+          TsmColorAnimatedDecorated(
+            duration: duration,
+            decoration: BoxDecoration(color: _decorationColor),
+            child: FlatButton(
+              onPressed: () {
+                setState(() {
+                  _decorationColor = Colors.red;
+                });
+              },
+              child: Text(
+                "AnimatedDecoratedBox",
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          )
+        ].map((e) {
+          return Padding(
+            padding: EdgeInsets.symmetric(vertical: 16),
+            child: e,
+          );
+        }).toList(),
+      ),
+    );
+  }
+}
+
 
 
 class StaggerRoute extends StatefulWidget {
